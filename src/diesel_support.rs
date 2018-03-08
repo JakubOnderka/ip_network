c26b2e4d739fb839abf4de957582625a6bad0e6b
Jakub Onderka <jakub.onderka@gmail.com>
2018-03-08 20:40:07 +0100

    Better error handling for empty input in Diesel FromSql

--- src/diesel_support.rs
+++ src/diesel_support.rs
@@ -2,29 +2,32 @@ use postgres_common;
 
 use {Ipv4Network, Ipv6Network, IpNetwork};
 use std::io::prelude::*;
+use std::error::Error;
 
 use diesel::deserialize::{self, FromSql};
 use diesel::serialize::{self, IsNull, Output, ToSql};
 use diesel::sql_types::Cidr;
 use diesel::pg::Pg;
 
+type BoxedError = Box<Error + Sync + Send>;
+
 impl FromSql<Cidr, Pg> for Ipv4Network {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        let raw = bytes.unwrap(); // TODO
+        let raw = bytes.ok_or::<BoxedError>("Input for Ipv4Network::from_sql is empty".into())?;
         postgres_common::from_sql_ipv4_network(raw)
     }
 }
 
 impl FromSql<Cidr, Pg> for Ipv6Network {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        let raw = bytes.unwrap(); // TODO
+        let raw = bytes.ok_or::<BoxedError>("Input for Ipv6Network::from_sql is empty".into())?;
         postgres_common::from_sql_ipv6_network(raw)
     }
 }
 
 impl FromSql<Cidr, Pg> for IpNetwork {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        let raw = bytes.unwrap(); // TODO
+        let raw = bytes.ok_or::<BoxedError>("Input for IpNetwork::from_sql is empty".into())?;
         match raw[0] {
             postgres_common::IPV4_TYPE => Ok(IpNetwork::V4(Ipv4Network::from_sql(bytes)?)),
             postgres_common::IPV6_TYPE => Ok(IpNetwork::V6(Ipv6Network::from_sql(bytes)?)),
