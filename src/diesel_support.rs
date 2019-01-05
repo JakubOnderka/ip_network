3dad0cd5b8568a834bb428641fc7b6f9d1785081
Jakub Onderka <jakub.onderka@gmail.com>
2019-01-05 23:33:32 +0100

    Simplified Diesel's FromSql by using not_none macro and return correct error for none input

--- src/diesel_support.rs
+++ src/diesel_support.rs
@@ -1,38 +1,33 @@
 use std::error::Error;
 use std::io::prelude::*;
-use diesel::deserialize::{self, FromSql};
 use diesel::expression::{AsExpression, Expression};
 use diesel::pg::Pg;
 use diesel::serialize::{self, IsNull, Output, ToSql};
+use diesel::deserialize::{self, FromSql};
 use diesel::sql_types::Cidr;
 use crate::{IpNetwork, Ipv4Network, Ipv6Network};
 use crate::postgres_common;
 
-type BoxedError = Box<Error + Sync + Send>;
-
 impl FromSql<Cidr, Pg> for Ipv4Network {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        bytes
-            .ok_or_else(|| "Input for Ipv4Network::from_sql is empty".into())
-            .and_then(postgres_common::from_sql_ipv4_network)
+        let bytes = not_none!(bytes);
+        postgres_common::from_sql_ipv4_network(bytes)
     }
 }
 
 impl FromSql<Cidr, Pg> for Ipv6Network {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        bytes
-            .ok_or_else(|| "Input for Ipv6Network::from_sql is empty".into())
-            .and_then(postgres_common::from_sql_ipv6_network)
+        let bytes = not_none!(bytes);
+        postgres_common::from_sql_ipv6_network(bytes)
     }
 }
 
 impl FromSql<Cidr, Pg> for IpNetwork {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-        let raw = bytes
-            .ok_or_else::<BoxedError, _>(|| "Input for IpNetwork::from_sql is empty".into())?;
-        match raw[0] {
-            postgres_common::IPV4_TYPE => Ok(IpNetwork::V4(Ipv4Network::from_sql(bytes)?)),
-            postgres_common::IPV6_TYPE => Ok(IpNetwork::V6(Ipv6Network::from_sql(bytes)?)),
+        let bytes = not_none!(bytes);
+        match bytes[0] {
+            postgres_common::IPV4_TYPE => Ok(IpNetwork::V4(postgres_common::from_sql_ipv4_network(bytes)?)),
+            postgres_common::IPV6_TYPE => Ok(IpNetwork::V6(postgres_common::from_sql_ipv6_network(bytes)?)),
             _ => Err("CIDR is not IP version 4 or 6".into()),
         }
     }
